30fe3af237bdc6d44581f64721fd59831f929ee6
WeXxi <wexxichannel@gmail.com>
2024-06-20 08:39:05 +0700

    Задание выполнено

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
@@ -15,22 +17,37 @@ public class Main {
 
     private static void divide() {
         //todo дописать логику и печать результата
+        if (b == 0){
+            System.out.println("Division by zero");
+        } else {
+            double divide = (a / b);
+            System.out.println(divide);
+        }
+
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        double diff = a - b;
+        System.out.println(diff);
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        double multiply = a * b;
+        System.out.println(multiply);
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        double sum = a + b;
+        System.out.println(sum);
     }
 
     private static double inputDouble() {
         //todo дописать логику
-        return 0;
+        Scanner sc = new Scanner(System.in);
+
+        return sc.nextDouble();
     }
 }
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    static int a = 0;
+
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        a = inputInt();
+        System.out.println(checkParity(a));
+    }
+
+    private static boolean checkParity(int a) {
+        return a % 2 == 0;
+    }
+
+    private static int inputInt() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
     }
 }
